9d70f0a1a6e649ce1c8d3b862bb3a19b583d2000
April Peng and Elysia Nason <april+elysia@>
2016-10-10 10:08:27 -0700

    add calculator functionality

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,6 +1,20 @@
 var Calculator = require('./../js/pingpong.js').calculatorModule;
 
 $(document).ready(function() {
+  $('#calculator-form').submit(function(event) {
+    event.preventDefault();
+    var calculatorFirst = parseInt($('#calculatorFirst').val());
+    var calculatorSecond = parseInt($('#calculatorSecond').val());
+    var calculatorType = $("input:radio[name='calculatorType']:checked").val();
+    var simpleCalculator = new Calculator("hot pink");
+    var calculatorOutput = simpleCalculator.calculator(calculatorFirst, calculatorType, calculatorSecond);
+    $('#solution').prepend("<p>" + calculatorOutput + "</p>");
+  });
+});
+
+var Calculator = require('./../js/pingpong.js').calculatorModule;
+
+$(document).ready(function() {
   $('#ping-pong-form').submit(function(event) {
     event.preventDefault();
     var goal = $('#goal').val();
